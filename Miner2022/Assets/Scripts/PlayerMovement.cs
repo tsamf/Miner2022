3e8df3cdc981a7e05f4aa5b38c68f37a1916af18
Tsamf <samfordt@gmail.com>
2023-01-03 21:16:29 -0600

    Added the player animator and added jump, run and idle animations

--- Miner2022/Assets/Scripts/PlayerMovement.cs
+++ Miner2022/Assets/Scripts/PlayerMovement.cs
@@ -5,35 +5,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float jumpSpeed = 5f;
-    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] float runSpeed = 10f;
 
     private Rigidbody2D myRigidbody2D;
+    private Vector2 moveInput;
+    private BoxCollider2D myBoxCollider2D;
+    private Animator myAnimator;
 
-    void Awake() {
-        myRigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        myRigidbody2D.velocity = new Vector2(moveInput.x * runSpeed, myRigidbody2D.velocity.y);
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(-Mathf.Sign(myRigidbody2D.velocity.x), 1f);
+        }
+
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
 
     void OnMove(InputValue value)
     {
-        Debug.Log("I moved!");
+        moveInput = value.Get<Vector2>();
     }
 
     void OnJump(InputValue value)
     {
-        myRigidbody2D.velocity += new Vector2(0, jumpSpeed);
-        Debug.Log("I jumped!");
+        if (!myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Platforms"))) { return; }
+
+        if (value.isPressed)
+        {
+            myRigidbody2D.velocity += new Vector2(0, jumpSpeed);
+            myAnimator.SetBool("isJumping", true);
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        myAnimator.SetBool("isJumping", false);
+    }
+
 }
